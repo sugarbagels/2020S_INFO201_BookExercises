090a91558c1c6ae67b0959c9ca5634f44dc22601
sugarbagels <snapyzorb@gmail.com>
2020-04-09 21:07:14 -0700

    Finished 5-1,6-1,6-2

--- chapter-04-exercises/exercise-2/README.md
+++ chapter-04-exercises/exercise-2/README.md
@@ -1,6 +1,12 @@
 # Exercise 2
 Using the text-edit of your choice (e.g., Atom), create a new file called `README.md`. Alternatively, you may clone this repository and edit this file.
+oh ok
+
 
 In your `README.md` document, make a three column table of the current courses you are taking, with separate columns for course number (in **bold**), course title (in _italics_), and instructor name.
 
 Note that you can preview this Markdown in Atom to make sure it works correctly.
+
+| Class # | Course Title | instructor name
+|**info201**| Infomratics | Kyle |
+|**ARTH484**| Art History | Melanie |
--- chapter-06-exercises/exercise-1/exercise.R
+++ chapter-06-exercises/exercise-1/exercise.R
@@ -2,34 +2,45 @@
 
 # Create a variable `my_name` that contains your name
 
+my_name <- "ana"
 
 # Create a variable `name_length` that holds how many letters (including spaces)
 # are in your name (use the `nchar()` function)
 
+name_length <- nchar(my_name)
 
 # Print the number of letters in your name
 
+print(name_length)
 
 # Create a variable `now_doing` that is your name followed by "is programming!" 
-# (use the `paste()` function)
+# (use the `paste()` faunction)
 
+
+now_doing <- paste(my_name,"is programming")
 
 # Make the `now_doing` variable upper case
 
-
+toupper(now_doing)
 ### Bonus
 
 # Pick two of your favorite numbers (between 1 and 100) and assign them to 
 # variables `fav_1` and `fav_2`
 
+fav_1 <- 69
+fav_2 <- 23
+
 
 # Divide each number by the square root of 201 and save the new value in the
 # original variable
 
+fav_1 <- fav_1 / sqrt(201)
+fav_2 <- fav_2 / sqrt(201)
 
 # Create a variable `raw_sum` that is the sum of the two variables. Use the 
 # `sum()` function for practice.
 
+raw_sum = fav_1 + fav_2 
 
 # Create a variable `round_sum` that is the `raw_sum` rounded to 1 decimal place.
 # Use the `round()` function.
